3a1bd38e28720a91b89f4b65e4afe6c586dc699b
sail pawar <sailpawarofficial@gmail.com>
2023-10-09 12:35:53 +0000

    trying casting, removed hook

--- models/staging/stg_fact_sales.sql
+++ models/staging/stg_fact_sales.sql
@@ -3,10 +3,10 @@ with required_columns as(
     {{ dbt_utils.generate_surrogate_key(['PROFIT_CENTER']) }} as fk_surr_pc_id,
     {{ dbt_utils.generate_surrogate_key(['CUSTOMER']) }} as fk_surr_cust_id,
     SALES_PERSON,
-    SALES_TARGET,
-    ACTUAL_SALES,
+    cast(SALES_TARGET as decimal(8,2)),
+    cast(ACTUAL_SALES as decimal(8,2)),
     {{month_short_to_day('TRANSACTION_MONTH')}} as transaction_month_in_number,
-    year
+    cast(year as int)
     from {{ ref('source_scd_2') }}
 ) select * from required_columns
 
